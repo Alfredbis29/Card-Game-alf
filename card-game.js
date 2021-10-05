6926ca25355f18629534ee45212c6f5bbe322183
Alfredbis29 <88894525+Alfredbis29@users.noreply.github.com>
2021-10-05 17:46:20 +0200

    Update card-game.js

--- card-game.js
+++ card-game.js
@@ -1,19 +1,9 @@
 import Deck from "./dark.js"
 
 const CARD_VALUE_MAP = {
-  "2": 2,
-  "3": 3,
-  "4": 4,
-  "5": 5,
-  "6": 6,
-  "7": 7,
-  "8": 8,
-  "9": 9,
-  "10": 10,
-  J: 11,
-  Q: 12,
-  K: 13,
-  A: 14
+  "2": 2, "3": 3, "4": 4, "5": 5,"6": 6,"7": 7,"8": 8, "9": 9, "10": 10,
+  J: 11,Q: 12, K: 13,  
+  A: 14  
 }
 
 const computerCardSlot = document.querySelector(".computer-card-slot")
